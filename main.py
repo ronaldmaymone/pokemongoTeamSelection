de423146a872d4cbcc13e7568dfb83c065dd3370
Ronald <ronaldmaymone@id.uff.br>
2022-02-01 18:32:37 -0300

    removendo lixo e tirando calcCP da batalha

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 from operator import itemgetter
 if __name__ == '__main__':
     # quantidade de instancias do problema. Cada instancia encontrar o melhor time B para enfrentar um dado time A fixo.
-    total_instance_number = 100
+    total_instance_number = 10
     current_instance = 1
     solutions = []
     timings = []
